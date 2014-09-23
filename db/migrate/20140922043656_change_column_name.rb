00dcd432838298c5726ad0b698994e4a195921f0
Joy Dunlap <joyceilidh@gmail.com>
2014-09-22 18:06:20 -0700

    commented migrate script for change_column_name

--- db/migrate/20140922043656_change_column_name.rb
+++ db/migrate/20140922043656_change_column_name.rb
@@ -1,5 +1,5 @@
-class ChangeColumnName < ActiveRecord::Migration
-  def change
-    rename_column :curriculums, :name, :title
-  end
-end
+#class ChangeColumnName < ActiveRecord::Migration
+#  def change
+#    rename_column :curriculums, :name, :title
+#  end
+#end
