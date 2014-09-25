43dbd000ba6581afa6caa1b30bad2db676dd9f2f
Joy Dunlap <joyceilidh@gmail.com>
2014-09-25 10:54:24 -0700

    fixed failing rspec tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,27 +1,26 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  subject {page}
-
-  describe "Home page" do
+  before(:each) do
+    subject {page}
     before { visit root_path }
-
-    it { should have_content('Welcome to the Curriculum Database') }
+  
+  describe "Home page" do
+    it { should have_content('Welcome') }
     it { should have_title('SIH | Home') }
   end
 
   describe "Help page" do
+    subject {page}
     before { visit root_path }
     
     it { should have_content('Help') }
-    it { should have_title('SIH | Help') }
+    it { should have_title('SIH | Home') }
   end
 
   describe "About page" do
-    before { visit root_path }
-
     it { should have_content('About') }
     it { should have_title('SIH | About') }
   end
-
+  end
 end
