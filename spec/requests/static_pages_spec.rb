be36fc987cd83d1820c10d3846f70e3a4e3efb76
Joy Dunlap <joyceilidh@gmail.com>
2014-09-24 18:04:11 -0700

    Static Pages Refactored with Titles

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Welcome to the Curriculum Database')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("SIH | Home")
+    end
   end
 
   describe "Help page" do
@@ -16,6 +21,11 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("SIH | Help")
+    end
   end
 
   describe "About page" do
@@ -23,6 +33,11 @@ describe "Static pages" do
     it "should have the content 'About SIH'" do
       visit '/static_pages/about'
       expect(page).to have_content('About SIH')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("SIH | About Us")
     end
   end
 
